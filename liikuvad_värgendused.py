aa76e19b684581a80b85349f6b9117cc2fb0820f
Gerten-Pilv7 <140986940+Gerten-Pilv7@users.noreply.github.com>
2023-10-16 23:38:41 +0300

    Create liikuvad_värgendused.py

---
+++ liikuvad_värgendused.py
@@ -0,0 +1,16 @@
+class player1:
+
+    def __init__(self, x_pos, y_pos):
+        self.x = x_pos
+        self.y = y_pos
+
+class player2:
+    def __init__(self, x_pos, y_pos):
+        self.x = x_pos
+        self.y = y_pos
+
+class ball:
+    def __init__(self, x_pos, y_pos, speed):
+        self.x = x_pos
+        self.y = y_pos
+        self.kiirus = speed
